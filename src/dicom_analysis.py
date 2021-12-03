8aa31f1d820fd52ae5f5da1fc3aba529e43d81e2
Ali Snedden <Ali.Snedden@nationwidechildrens.org>
2021-12-02 22:18:02 -0500

    .gitignore : 1. Ignore __pycache__
    src/dicom_analysis.py :
    1. Added ability to plot a series of dicom images OR a single dicom image
        a) Added switch at CL to specify this
    2. Added plot_multiple_dicom() func to plot series of dicom images

--- src/dicom_analysis.py
+++ src/dicom_analysis.py
@@ -11,8 +11,10 @@
 import sys
 import numpy as np
 import time
+import glob
 from matplotlib import pyplot as plt
 from matplotlib.ticker import FormatStrFormatter
+from matplotlib.widgets import Slider
 import pydicom
 # my code
 from error import exit_with_error
@@ -32,13 +34,15 @@ def print_help(ExitVal=None):
     FUTURE:
     """
     sys.stdout.write(
-        "\nUSAGE : python src/dicom_analysis.py some_file.dcm\n\n"
-        "      some_file.dcm : A DICOM file\n"
+        "\nUSAGE : python src/dicom_analysis.py path [series|single]\n\n"
+        "      path            : string, path to either a DICOM file or a directory of DICOMs\n"
+        "      [series|single] : string, if 'series', path is a directory with a series of DICOM files\n"
+        "                      : string, if 'single', path is a single DICOM file\n"
         "                         \n")
     sys.exit(ExitVal)
 
 
-def plot_dicom(PixelM=None):
+def plot_single_dicom(PixelM=None):
     """
     ARGS:
         PixelM  :  2D - Numpy array extacted from Dicom file
@@ -56,6 +60,37 @@ def plot_dicom(PixelM=None):
     fig.colorbar(im, ax=ax)
 
     #plt.tight_layout()
+    plt.show()
+    #plt.close(fig)
+
+
+def plot_multiple_dicom(PixelT=None):
+    """
+    ARGS:
+        PixelT  :  3D - Numpy array extacted from Dicom file
+    DESCRIPTION:
+        Plots PixelT as a heat map with slider to sweep through
+        the z-axis
+    RETURN:
+        N/A
+    DEBUG:
+    FUTURE:
+    """
+    fig, ax = plt.subplots(nrows=1, ncols=1)
+    im = ax.imshow(X=PixelT[:,:,0], cmap='bone', interpolation='none')
+    # Create sliders
+    sliderAx = fig.add_axes([0.13, 0.025, 0.56, 0.02])
+    slider = Slider(ax=sliderAx, label='z', valmin=0, valmax=PixelT.shape[2]-1, valinit=0)
+    ax.set_title("Plot of Dicom data")
+    fig.colorbar(im, ax=ax)
+    
+    # Define how slider behaves
+    def update_slider(val):
+        z = int(round(slider.val))
+        im = ax.imshow(X=PixelT[:,:,z], cmap='bone', interpolation='none')
+
+    #plt.tight_layout()
+    slider.on_changed(update_slider)
     plt.show()
     #plt.close(fig)
 
@@ -78,20 +113,50 @@ def main():
                         "expected\n".format(sys.version_info[0]))
 
     ###### Get Command Line Options ######
-    if(len(sys.argv) != 2) :
+    if(len(sys.argv) != 2 and len(sys.argv) != 3) :
         print_help(1)
     elif(len(sys.argv) == 2 and "-h" in sys.argv[1]):
         print_help(0)
+    elif(len(sys.argv) != 3):
+        print_help(1)
+
     path = sys.argv[1]
+    if(sys.argv[2].lower() == "series"):
+        isSeries = True
+    elif(sys.argv[2].lower() == "single"):
+        isSeries = False
+    else:
+        exit_with_error("ERROR!!! Invalid value for [series|single]\n")
 
     print("Started : %s"%(time.strftime("%D:%H:%M:%S")))
     startTime = time.time()
 
     # Extract data
-    data = pydicom.dcmread(path)
-    pixelM = data.pixel_array
+    if(isSeries == True):
+        # Get list of DICOM files
+        fileL = glob.glob("{}/*.dcm".format(path))      # presumably have dcm suffix...
+        dim = None
+        # Assume all files should have the same dimension...
+        # --> allocate 3D array
+        data = pydicom.dcmread(fileL[0])
+        shape = data.pixel_array.shape
+        pixelT = np.zeros([shape[0], shape[1], len(fileL)])      # 3D matrix, or Tensor
+        idx=0                   # File index
+        fileL = sorted(fileL)
+        for fPath in fileL:
+            data = pydicom.dcmread(fPath)
+            pixelM = data.pixel_array
+            if(shape != pixelM.shape):
+                exit_with_error("ERROR!!! {} contains DICOM files of different "
+                                "dimensions! {} != {}\n".format(fPath,shape,data.shape))
+            pixelT[:,:,idx] = pixelM
+            idx+=1
+        plot_multiple_dicom(PixelT=pixelT)
 
-    plot_dicom(PixelM=pixelM)
+    else:
+        data = pydicom.dcmread(path)
+        pixelM = data.pixel_array
+        plot_single_dicom(PixelM=pixelM)
 
     
     print("Ended : %s"%(time.strftime("%D:%H:%M:%S")))
